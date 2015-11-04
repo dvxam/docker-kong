28b23c10c8c8fe5656f7f121d36482308a77f9f2
dvxam <dvxam@laposte.net>
2015-11-04 11:12:57 +0100

    Funtional plugin

--- kong/plugins/token-validator/access.lua
+++ kong/plugins/token-validator/access.lua
@@ -1,4 +1,5 @@
 local responses = require "kong.tools.responses"
+local http_client = require "kong.tools.http_client"
 
 local _M = {}
 
@@ -13,6 +14,15 @@ local function token_empty(token)
   return false
 end
 
+local function check_token_validity(auth_token, config)
+  local url = "http://mrdrive-pimp-my-auth-staging.herokuapp.com/session"
+  local headers = {
+    ["Content-Type"] = "application/json",
+    [config.header] = auth_token
+  }
+  return http_client.get(url, {}, headers)
+end
+
 function _M.execute(config)
   local auth_token = extract_auth_token(config)
 
@@ -20,6 +30,22 @@ function _M.execute(config)
   if token_empty(auth_token) then
     ngx.ctx.stop_phases = true
     return responses.send_HTTP_UNAUTHORIZED()
+  end
+
+  -- Actually call the Auth API to get session status and user data
+  _, status, headers = check_token_validity(auth_token, config)
+
+  -- Return 401 Unauthorized if token is not valid
+  if status ~= 200 then
+    ngx.ctx.stop_phases = true
+    return responses.send_HTTP_UNAUTHORIZED()
+  end
+
+  -- Add all headers matching X-AUTH-DATA-* to request
+  for name, value in pairs(headers) do
+    if name:upper():match("X%-AUTH%-DATA%-") then
+      ngx.req.set_header(name, value)
+    end
   end
 end
 
